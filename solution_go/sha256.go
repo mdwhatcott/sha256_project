876de5ccacc54473bc9069defb349b05f04fd47f
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2022-06-18 10:13:57 -0600

    Inline binary conversion.

--- solution_go/sha256.go
+++ solution_go/sha256.go
@@ -1,7 +1,5 @@
 package main
 
-import "encoding/binary"
-
 func rightRotate32(x uint32, n int) uint32 {
 	right := x >> n
 	left := x << (32 - n)
@@ -20,7 +18,8 @@ func messageSchedule(input string) (result []uint32) {
 	result = make([]uint32, 64)
 	for i := 0; i < 16; i++ {
 		n := i * 4
-		result[i] = binary.BigEndian.Uint32([]byte(input[n : n+4]))
+		b := input[n : n+4]
+		result[i] = uint32(b[3]) | uint32(b[2])<<8 | uint32(b[1])<<16 | uint32(b[0])<<24
 	}
 	for i := 16; i < 64; i++ {
 		result[i] = result[i-16] + littleSigma0(result[i-15]) + result[i-7] + littleSigma1(result[i-2])
