1c6a0c8559bff84e7e2e69a83ad4016ab4d7d437
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2022-06-17 22:49:04 -0600

    Initial commit for solution in Go.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mdwhatcott/sha256_project
+
+go 1.18
---
+++ solution_go/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"os"
+)
+
+func main() {
+	var problems Problems
+	err := json.NewDecoder(os.Stdin).Decode(&problems)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// pprint(problems)
+
+	var results Solutions
+
+	// TODO: solve!
+
+	err = json.NewEncoder(os.Stdout).Encode(results)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func pprint(problems any) {
+	var pretty bytes.Buffer
+	ugly, err := json.Marshal(problems)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Indent(&pretty, ugly, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(pretty.String())
+}
+
+type Problems struct {
+	Problem1  [][]int
+	Problem2  [][]int
+	Problem3  int
+	Problem4  int
+	Problem5  string
+	Problem6  int
+	Problem7  int
+	Problem8  []int
+	Problem9  []int
+	Problem10 Problem10
+	Problem11 Problem11
+	Problem12 []int
+	Problem13 []string
+	Problem14 Problem14
+	Problem15 string
+	Problem16 Problem16
+}
+
+type Problem10 struct {
+	State         []int
+	RoundConstant int `json:"round_constant"`
+	ScheduleWord  int `json:"schedule_word"`
+}
+
+type Problem11 struct {
+	State []int
+	Block string
+}
+
+type Problem14 struct {
+	OriginalInput string `json:"original_input"`
+	ChosenSuffix  string `json:"chosen_suffix"`
+}
+
+type Problem16 struct {
+	OriginalHash   string `json:"original_hash"`
+	OriginalLength int    `json:"original_length"`
+	ChosenSuffix   string `json:"chosen_suffix"`
+}
+
+type Solutions struct {
+	Problem1  []int    `json:"problem1,omitempty"`
+	Problem2  []int    `json:"problem2,omitempty"`
+	Problem3  int      `json:"problem3,omitempty"`
+	Problem4  int      `json:"problem4,omitempty"`
+	Problem5  []int    `json:"problem5,omitempty"`
+	Problem6  int      `json:"problem6,omitempty"`
+	Problem7  int      `json:"problem7,omitempty"`
+	Problem8  int      `json:"problem8,omitempty"`
+	Problem9  int      `json:"problem9,omitempty"`
+	Problem10 []int    `json:"problem10,omitempty"`
+	Problem11 []int    `json:"problem11,omitempty"`
+	Problem12 []string `json:"problem12,omitempty"`
+	Problem13 []string `json:"problem13,omitempty"`
+	Problem14 string   `json:"problem14,omitempty"`
+	Problem15 []int    `json:"problem15,omitempty"`
+	Problem16 string   `json:"problem16,omitempty"`
+}
